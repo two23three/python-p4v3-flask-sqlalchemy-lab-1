06615a80f678bda2eac7181a0621c6def47410fc
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-06-28 10:51:35 +0300

    done

--- server/app.py
+++ server/app.py
@@ -21,7 +21,22 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    if earthquake:
+        return earthquake.to_dict(), 200
+    return {"message": "Earthquake 9999 not found."}, 404
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    earthquakes = [earthquake.to_dict() for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()]
+    count = len(earthquakes)
+    response_body = {
+        'count': count,
+        'quakes': earthquakes
+    }
+    return response_body, 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
